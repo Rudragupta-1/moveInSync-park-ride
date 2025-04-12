23541edebfc0d6cd1e07fc033b5a52886c316dbf
Rudra Gupta <guptarudra901@gmail.com>
2025-04-12 16:45:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,17 +24,17 @@ dotenv.config();
 const app = express();
 
 // Rate Limiting Middleware
-// const apiLimiter = rateLimit({
-//   windowMs: 15 * 60 * 1000, 
-//   max: 100, 
-//   standardHeaders: true,
-//   legacyHeaders: false,
-//   message: {
-//     success: false,
-//     message: 'Too many requests from this IP, please try again after 15 minutes'
-//   }
-// });
-// app.use('/api', apiLimiter); 
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, 
+  max: 100, 
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: {
+    success: false,
+    message: 'Too many requests from this IP, please try again after 15 minutes'
+  }
+});
+app.use('/api', apiLimiter); 
 
 // Middleware
 app.use(cors());
